85718103d85ccc5813a4608b14682a3fcecb5fc4
rpratama94 <rpratama.sky@gmail.com>
2020-12-24 08:03:16 +0700

    Setup Client Web

---
+++ src/helpers/callServer.v2.js
@@ -0,0 +1,44 @@
+import { axios } from './Axios';
+
+/**
+ *
+ * @param {url, stage, method, data, headers, type, id} option
+ */
+export default function callServerV2(option) {
+  const payloadAxios = {
+    url: option.url,
+    method: option.method || 'GET',
+  };
+
+  if (option.data) {
+    payloadAxios['data'] = option.data;
+  }
+
+  if (option.headers) {
+    payloadAxios['headers'] = {
+      access_token: localStorage.getItem('access_token'),
+    };
+  }
+
+  return async (dispatch) => {
+    try {
+      dispatch({ type: option.type + '_LOADING', payload: true });
+      const { data } = await axios(payloadAxios);
+      // console.log('Data From Axios: ' + option.stage, data);
+      dispatch({
+        type: option.type,
+        stage: option.stage,
+        payload: option.id ? option.id : option.deletedId ? option.deletedId : data,
+      });
+    } catch (error) {
+      // console.error('axios', error.message, 'stage', option.stage);
+      dispatch({
+        type: option.type + '_ERROR',
+        stage: option.stage,
+        payload: error,
+      });
+    } finally {
+      dispatch({ type: option.type + '_LOADING', payload: false });
+    }
+  };
+}
---
+++ src/store/reducers/reducerUser.js
@@ -0,0 +1,41 @@
+const INIT = {
+  users: [],
+  user: null,
+  loading: false,
+  error: null,
+  stage: null,
+};
+
+export default function reducer(state = INIT, action) {
+  switch (action.type) {
+    case 'SET_USERS':
+      return { ...state, users: action.payload, error: null, stage: action.stage };
+    case 'SET_USER':
+      return { ...state, user: action.payload, error: null, stage: action.stage };
+    case 'SET_USERS_LOADING':
+    case 'SET_USER_LOADING': {
+      const newStateLoading = {
+        ...state,
+        loading: action.payload,
+      };
+      newStateLoading.users = action.type === 'SET_USERS' && action.payload && null;
+      newStateLoading.user = action.type === 'SET_USER' && action.payload && null;
+      newStateLoading.error = action.payload && null;
+      return newStateLoading;
+    }
+    case 'SET_USERS_ERROR':
+    case 'SET_USER_ERROR': {
+      const newStateError = {
+        ...state,
+        stage: action.stage,
+        error: action.payload,
+      };
+
+      newStateError.users = action.type === 'SET_USERS' && null;
+      newStateError.user = action.type === 'SET_USER' && null;
+      return newStateError;
+    }
+    default:
+      return state;
+  }
+}
