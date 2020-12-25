685e7e06e6890585d52e5a02e5d1d6ba59244e6b
rpratama94 <rpratama.sky@gmail.com>
2020-12-25 09:27:45 +0700

    Change App Name

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, useHistory } from 'react-router-dom';
-import Logo from '../logo.svg';
+import Logo from '../assets/images/logo.png';
 import Avatar from '../assets/images/avatar.png';
 // import { actionSetLogin } from '../store/actions';
 
